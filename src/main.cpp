124b01a0378798375086e9add2e0408d65a3337b
Jakub Górecki <jakub.xiz1.gorecki@gmail.com>
2023-04-20 22:08:18 +0200

    Created: Main menu code workflow.

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,37 @@
 #include "log_in_system.h"
+#include <conio.h>
 
 int main()
 {
-	std::cout << "Hello World!\n";
+	system("cls");
+	cout << "1. LOG IN\n";
+	cout << "1. SIGN IN\n";
+	cout << "1. EXIT\n";
+
+	switch (_getch())
+	{
+		case '1':
+		{
+			log_in_system start;
+			start.log_in();
+		}break;
+
+		case '2':
+		{
+			log_in_system start;
+			start.sign_in();
+		}break;
+
+		case '3':
+		{
+
+		}break;
+
+		default:
+		{
+			
+		}
+	}
 
 	return 0;
 }
